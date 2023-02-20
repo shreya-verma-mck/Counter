9a0d6cd27e515b4c87df7df63a53e4810389ddb3
Aishwarya S R <Aishwarya.SR>
2023-02-20 10:09:38 +0530

    chore(counter): update unit test cases

--- src/components/Counter/__tests__/index.test.jsx
+++ src/components/Counter/__tests__/index.test.jsx
@@ -8,33 +8,32 @@ describe("Counter", () => {
     const { asFragment } = render(<Counter />);
     expect(asFragment()).toMatchSnapshot();
   });
-  it("should display the initial counter value as 0", () => {
-    const { getByTestId } = render(<Counter />);
-    const counterValue = getByTestId("counter-value");
-    expect(counterValue).toHaveTextContent(0);
+  it("should display the counter value as 0 when the page is loaded", () => {
+    const { queryByText } = render(<Counter />);
+    expect(queryByText(0)).toBeTruthy();
   });
   it("should increment the counter value by 1 when the plus button is clicked", () => {
-    const { getByTestId } = render(<Counter />);
-    const plusButton = getByTestId("plus-button");
+    const { getByText, queryByText } = render(<Counter />);
+    expect(queryByText("1")).toBeFalsy();
+    const plusButton = getByText("+");
     fireEvent.click(plusButton);
-    const counterValue = getByTestId("counter-value");
-    expect(counterValue).toHaveTextContent(1);
+    expect(queryByText("1")).toBeTruthy();
   });
-  it("should decrement the counter value by 1 when the counter value is greater than 1 and minus icon is clicked", () => {
-    const { getByTestId } = render(<Counter />);
-    const plusButton = getByTestId("plus-button");
+  it("should decrement the counter value by 1 when the counter value is greater than 1 and minus button is clicked", () => {
+    const { getByText, queryByText } = render(<Counter />);
+    const plusButton = getByText("+");
     fireEvent.click(plusButton);
     fireEvent.click(plusButton);
-    const minusButton = getByTestId("minus-button");
+    fireEvent.click(plusButton);
+    expect(queryByText("2")).toBeFalsy();
+    const minusButton = getByText("-");
     fireEvent.click(minusButton);
-    const counterValue = getByTestId("counter-value");
-    expect(counterValue).toHaveTextContent(1);
+    expect(queryByText("2")).toBeTruthy();
   });
-  it("should not decrement the counter value by 1 when the counter value is 0 and the minus icon is clicked", () => {
-    const { getByTestId } = render(<Counter />);
-    const minusButton = getByTestId("minus-button");
+  it("should not decrement the counter value by 1 when the counter value is 0 and the minus button is clicked", () => {
+    const { getByText, queryByText } = render(<Counter />);
+    const minusButton = getByText("-");
     fireEvent.click(minusButton);
-    const counterValue = getByTestId("counter-value");
-    expect(counterValue).toHaveTextContent(0);
+    expect(queryByText("0")).toBeTruthy();
   });
 });
